dccc22c3b57b7c865d7723713164c32741fdef3d
hongthuan <1751010150thuan@ou.edu.vn>
2019-12-13 14:58:42 -0500

    chore

--- src/MainPage/Contact.js
+++ src/MainPage/Contact.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet, Image, Dimensions } from 'react-native'
+import {Icon, } from  'native-base';
+
 
 export default class Contact extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Image source={require('../../static/react-native.png')} style={styles.img} />
-                <View style={styles.contact}>
-                    <Text></Text>
-                    <Text></Text>
+                <View style={{flex: 1}}>
+                    <Image source={require('../../static/react-native.png')} style={styles.img} />
                 </View>
-                
+                <View style={styles.contact}>
+                    <View style={styles.field}>
+                        <Icon name="user" type="AntDesign"/>
+                        <Text>Hong Thanh Thuan</Text>
+                    </View>
+                    <View style={styles.field}>
+                        <Icon name="location-on" type="MaterialIcons"/>
+                        <Text>Ho Chi Minh City</Text>
+                    </View>
+                    <View style={styles.field}>
+                        <Icon name="email" type="MaterialCommunityIcons"/>
+                        <Text>thuanhong357@gmail.com</Text>
+                    </View>
+                </View>
             </View>
         )
     }
@@ -23,10 +36,15 @@ const styles = StyleSheet.create({
         alignContent: 'stretch'
     },
     img: {
-        width: Dimensions.get('window').width * 1,
-        height: Dimensions.get('window').height * 0.4
+        width: Dimensions.get('screen').width,
+        height: Dimensions.get('screen').height * 0.3,
     },
     contact: {
         flex: 1
+    },
+    field: {
+        flex : 1,
+        flexDirection: 'row',
+        
     }
 })
